f614238b1b4f0d9d7c6ef88e95b9e8e67702d691
Sudipta Das <shiningsudipto@gmail.com>
2024-10-02 12:57:40 +0600

    commenting post functionality done

--- src/app/(commonLayout)/articles/[postID]/page.tsx
+++ src/app/(commonLayout)/articles/[postID]/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 import DownVote from "@/src/components/module/articles/DownVote";
 import UpVote from "@/src/components/module/articles/UpVote";
-import { useGetCommentsByPostIdQuery } from "@/src/redux/features/comment";
+import {
+  useGetCommentsByPostIdQuery,
+  usePostCommentMutation,
+} from "@/src/redux/features/comment";
 import { useGetPostDetailsQuery } from "@/src/redux/features/post";
 import { TComment, TPostDetails } from "@/src/types";
 import { formatDateTime } from "@/src/utils/date";
@@ -10,6 +13,11 @@ import Link from "next/link";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { LiaUserEditSolid } from "react-icons/lia";
 import { IoIosTimer } from "react-icons/io";
+import { Form, Formik } from "formik";
+import Textarea from "@/src/components/formik/Textarea";
+import { Button } from "@nextui-org/button";
+import { useAppSelector } from "@/src/redux/hooks";
+import { TUser, useCurrentUser } from "@/src/redux/features/auth/authSlice";
 
 interface TProps {
   params: {
@@ -21,6 +29,28 @@ const PostDetails = ({ params }: TProps) => {
   const { data, isLoading } = useGetPostDetailsQuery(id);
   const postInfo = data?.data as TPostDetails;
   const { data: allComments } = useGetCommentsByPostIdQuery(id);
+  const [createComment] = usePostCommentMutation();
+
+  const user = useAppSelector(useCurrentUser) as TUser;
+  console.log("user:", user);
+
+  const onSubmit = async (values) => {
+    console.log(values);
+    const commentData = {
+      postId: postInfo._id,
+      userId: user.id,
+      ...values,
+    };
+    try {
+      const res = await createComment({
+        id: postInfo._id,
+        commentInfo: commentData,
+      }).unwrap();
+      console.log("res", res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   if (isLoading) {
     return <p>Loading...</p>;
   }
@@ -56,29 +86,45 @@ const PostDetails = ({ params }: TProps) => {
             <FaRegCommentAlt /> {postInfo.commentsCount}
           </p>
         </div>
-        <div className="lg:w-[50%] mt-5 bg-slate-900">
-          {allComments?.data?.map((item: TComment) => {
-            return (
-              <div key={item?._id} className="border rounded-xl p-5">
-                <div className="flex items-center gap-5 mb-4">
-                  <Image
-                    src={item.userId.avatar}
-                    height={80}
-                    width={80}
-                    alt={item.userId.name}
-                    className="rounded-full size-[40px] object-cover"
-                  />
-                  <div>
-                    <Link href={item.userId._id} className="font-bold">
-                      {item?.userId.name}
-                    </Link>
-                    <p>{formatDateTime(item.createdAt)}</p>
-                  </div>
+        <div className="lg:w-[50%] mt-5">
+          <div className="py-10">
+            <Formik initialValues={{ feedback: "" }} onSubmit={onSubmit}>
+              <Form className="space-y-5">
+                <p className="text-xl font-bold">Share your opinion</p>
+                <Textarea name="feedback" />
+                <div className="flex justify-end">
+                  <Button type="submit">Post</Button>
                 </div>
-                <p>{item?.feedback}</p>
-              </div>
-            );
-          })}
+              </Form>
+            </Formik>
+          </div>
+          <div className="space-y-5">
+            {allComments?.data?.map((item: TComment) => {
+              return (
+                <div
+                  key={item?._id}
+                  className="border rounded-xl p-5 bg-slate-900"
+                >
+                  <div className="flex items-center gap-5 mb-4">
+                    <Image
+                      src={item.userId.avatar}
+                      height={80}
+                      width={80}
+                      alt={item.userId.name}
+                      className="rounded-full size-[40px] object-cover"
+                    />
+                    <div>
+                      <Link href={item.userId._id} className="font-bold">
+                        {item?.userId.name}
+                      </Link>
+                      <p>{formatDateTime(item.createdAt)}</p>
+                    </div>
+                  </div>
+                  <p>{item?.feedback}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
--- src/redux/features/comment/index.ts
+++ src/redux/features/comment/index.ts
@@ -4,8 +4,21 @@ const CommentApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getCommentsByPostId: builder.query({
       query: (id) => `/comment/post/${id}`,
+      providesTags: ["Comment"],
+    }),
+    postComment: builder.mutation({
+      query: ({ id, commentInfo }) => {
+        // console.log(id, commentInfo);
+        return {
+          url: `/comment/${id}`,
+          method: "POST",
+          body: commentInfo,
+        };
+      },
+      invalidatesTags: ["Comment"],
     }),
   }),
 });
 
-export const { useGetCommentsByPostIdQuery } = CommentApi;
+export const { useGetCommentsByPostIdQuery, usePostCommentMutation } =
+  CommentApi;
