80d502af25155619a648e3e1914d80038bf48536
Sudipta Das <shiningsudipto@gmail.com>
2024-10-06 01:06:52 +0600

    contact us page added

--- src/app/(commonLayout)/contact-us/page.tsx
+++ src/app/(commonLayout)/contact-us/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+
 const page = () => {
   return (
     <div className="px-4 py-10 dark:bg-dark">
@@ -10,29 +13,16 @@ const page = () => {
       {/* Contact Form */}
       <form className="max-w-md mx-auto">
         <div className="mb-4">
-          <label htmlFor="name" className="block text-gray-700">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="border rounded-lg w-full p-2"
-            required
-          />
+          <Input type="text" label="Name" />
         </div>
         <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-700">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            className="border rounded-lg w-full p-2"
-            required
-          />
+          <Input type="email" label="Email" />
         </div>
         <div className="mb-4">
-          <label htmlFor="message" className="block text-gray-700">
+          <label
+            htmlFor="message"
+            className="block text-gray-700 dark:text-slate-100"
+          >
             Message
           </label>
           <textarea
@@ -42,12 +32,13 @@ const page = () => {
             required
           ></textarea>
         </div>
-        <button
+        <Button
+          fullWidth
           type="submit"
-          className="bg-blue-500 text-white rounded-lg px-4 py-2"
+          className=" text-white rounded-lg px-4 py-2"
         >
           Send Message
-        </button>
+        </Button>
       </form>
 
       {/* Support Highlight Section */}
